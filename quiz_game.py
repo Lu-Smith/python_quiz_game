8d810990f6a0beb70216f18105518fba6344ed61
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-07 23:35:44 +0100

    Updated logic

--- quiz_game.py
+++ quiz_game.py
@@ -7,7 +7,30 @@ if playing != "yes":
 
 print("Okay! Let's play :)")
 
-answer = input("What does CPU stand for? ")
+answer0 = input("What does CPU stand for? ")
 
-if answer.upper() == "CENTRAL PROCESSING UNIT":
+if answer0.upper() == "CENTRAL PROCESSING UNIT":
     print("Correct!")
+else:
+    print("Incorrect!")
+
+answer1 = input("What does GPU stand for? ")
+
+if answer1.upper() == "GRAPHICS PROCESSING UNIT":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer2 = input("What does RAM stand for? ")
+
+if answer2.upper() == "RANDOM ACCESS MEMORY":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer3 = input("What does PSU stand for? ")
+
+if answer3.upper() == "POWER SUPPLY UNIT":
+    print("Correct!")
+else:
+    print("Incorrect!")
