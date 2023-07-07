3d39caa52c18b0fa510076fb7624790683c96866
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-07-07 23:45:54 +0100

    Updated logic

--- quiz_game.py
+++ quiz_game.py
@@ -41,4 +41,6 @@ if answer3.upper() == "POWER SUPPLY UNIT":
 else:
     print("Incorrect!")
 
-print("You got " + str(score) + "questions correct!")
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 4) * 100) + "%.")
+
